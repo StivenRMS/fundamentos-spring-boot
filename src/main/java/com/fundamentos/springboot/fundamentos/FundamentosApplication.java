9e018dda2e1b14939d8a0f56376c5d711117244d
Stiven Ramos <StivenRMS>
2023-05-13 22:50:28 -0600

    Implementacion Propia

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,30 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
+
+	private ComponentDependency componentDependency;
+	private MyBean myBean;
+	public FundamentosApplication( @Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean ){ //con @Qualifier se dice cual de las dos dependecia que queremos llamr
+		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		componentDependency.saludar();
+		myBean.print();
+	}
 }
